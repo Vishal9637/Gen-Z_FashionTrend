df461c00f6159f1ff69bc60999699509286c56ab
Vishal <gadagevishal9@gmail.com>
2026-01-06 21:25:05 +0530

    ok

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
+
 import Navbar from "./components/Navbar";
 import Home from "./Pages/Home.jsx";
 import Results from "./Pages/Results.jsx";
@@ -6,15 +7,17 @@ import OutfitDetails from "./Pages/OutfitDetails.jsx";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/results" element={<Results />} />
         <Route path="/outfit/:id" element={<OutfitDetails />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
 export default App;
+
+
